289f3fb9549d4104f3a27560134055de46cb8397
carlos8788 <carlos8788@gmail.com>
2023-08-09 07:33:31 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+const PORT = 3000
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+
+app.listen(PORT, ()=>{
+    console.log(`PORT: ${PORT}`);
+});
