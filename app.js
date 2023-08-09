2c36a5ec89ae80d80a7126cf3d4a1f205a2cf05b
carlos8788 <carlos8788@gmail.com>
2023-08-09 08:00:39 -0300

    process.env.PORT

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
